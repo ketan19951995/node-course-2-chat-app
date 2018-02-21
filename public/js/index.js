e58ef69ecdc9bac12606a29b9c44a5c11785abe5
ketan95 <ketant.webappmate@gmail.com>
2018-02-21 19:19:01 +0530

    file commit by ketan

---
+++ public/js/index.js
@@ -0,0 +1,48 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
+});
+
+
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click' , function(){
+ if (!navigator.geolocation){
+    return alert('GEOLACATION not Suppported by your browser');
+
+
+ }
+
+ navigator.geolocation.getCurrentPosition(function (position){
+ console.log(position);
+
+ }, function(){
+  alert('Unable to fetch location');
+  
+ });
+
+});
